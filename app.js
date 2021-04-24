a82300a449a4bdb790c6419ad3381a5b089451c0
Walter Arce <walter.arce@inworx.com>
2021-04-24 12:24:17 -0300

    se agrega comportamiento para acceder a api

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 angular.module('app',[])
 .component('cuerpo',{
-    templateUrl:'cuerpocmp.html',
-    controller : function cuerpoController(){
-        console.log('cuerpo');
-    }
-})
+    templateUrl:'cuerpocmp.html'
+
+}) 
+.controller('starshipsCtrl', function($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'https://swapi.dev/api/starships/',
+        data: 'parameters'
+        }).then(function success(response) {
+            $scope.starships =[];
+            $scope.starships= response.data.results;
+            $scope.statusval = response.status;
+            $scope.statustext = response.statusText;
+            $scope.headers = response.headers();
+        }, function error(response) {
+        });
+  })
 .component('listado',{
-    templateUrl: 'listadocmp.html',
-    controller : function listadoController(){
-       var valor = 'walter';
-        console.log('text'+valor);    
-    }
-});
+    templateUrl: 'listadocmp.html'
+    })
+.controller('bodyctrl', function() {
+    var vm = this;
+    vm.message = 'Hi from home';
+    console.log(vm.message);
+  });
